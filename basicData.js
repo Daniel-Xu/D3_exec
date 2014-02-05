188460bad8ec151b93b7effd9d52642dc78857b8
Daniel-Xu <xwd.forever@gmail.com>
2014-02-05 12:23:20 -0500

    enter update and exit
    after enter(), the join data will become the whole dataset

--- basicData.js
+++ basicData.js
@@ -1,7 +1,7 @@
 var dataset = [18, 29, 34, 22, 12]
 
 svg = d3.select("body").append("svg").attr({
-    width: 600, 
+    width: 1000, 
     height: 400
 })
 
@@ -18,4 +18,22 @@ svg.selectAll("rect")
         fill: "red"
     })
     
+    
+var moreData = [11, 12, 13, 14, 15, 16, 17]
+var join = svg.selectAll("rect").data(moreData)
 
+// after enter(), only new added rect will be affected
+join.enter()
+    .append("rect")
+    .attr({
+        x:function(d, i){ return 101*i},
+        y: function(d){ return 400-d*5},
+        width:100,
+        height: function(d) { return d*5}, 
+        fill: "yellow"
+    })
+
+//noting this: the join now becomes the whole 7 rects
+join.attr({
+    fill: "blue" 
+})
