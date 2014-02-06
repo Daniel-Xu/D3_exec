668281976e4ad8f2abf81af8df20bfe836c9df84
Daniel-Xu <xwd.forever@gmail.com>
2014-02-05 23:45:12 -0500

    write basic scatter circle
    d3.max(data, function(d){ return d.x})

---
+++ axes.js
@@ -0,0 +1,52 @@
+var w = window.innerWidth
+var h = window.innerHeight
+var range = { top: 40, right: 20, bottom: 20, left: 40}
+var radius = 6
+
+
+var svg = d3.select("body").append("svg").attr({
+    width: w,
+    heigth: h
+})
+    
+var data = [
+    {x: 100, y: 110},
+    {x: 83, y: 43},
+    {x: 92, y: 28},
+    {x: 49, y: 74},
+    {x: 51, y: 10},
+    {x: 25, y: 98},
+    {x: 77, y: 30},
+    {x: 20, y: 83},
+    {x: 11, y: 63},
+    {x: 4,  y: 55},
+    {x: 0,  y: 0},
+    {x: 85, y: 100},
+    {x: 60, y: 40},
+    {x: 70, y: 80},
+    {x: 10, y: 20},
+    {x: 40, y: 50},
+    {x: 25, y: 31}
+]
+
+var xScale = d3.scale.linear()
+                .domain([0, d3.max(data, function(d){return d.x})])
+                .range([range.left, w-range.right])
+
+var yScale = d3.scale.linear()
+                .domain([0, d3.max(data, function(d){ return d.y})])
+                .range([range.top, h-range.bottom])
+
+
+svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr({
+        cx: function(d){ return xScale(d.x)},
+        cy: function(d){ return yScale(d.y)}, 
+        r: radius
+    })
+
+
+
